a7de3dafe6604a8a70ca9dcbf07cfe8c1a2f63e4
nagamalleswarieluri <nagaeluri44@gmail.com>
2025-10-24 12:29:54 +0530

    Add files via upload

---
+++ oopsintro.py
@@ -0,0 +1,127 @@
+# class student:
+#     def __init__(self,name,rollnum):
+#         self.name=name
+#         self.rollnum=rollnum
+#     def display(self):
+#         print('student Name:',self.name)
+#         print('student Rollnum:',self.rollnum)
+# std1=student('malleswari',34)
+# std1.display()
+# std1=student('lakshmi', 22)
+# std1.display()
+
+        
+# class car:
+#     def __init__(self,brand,model):
+#         self.brand=brand
+#         self.model=model
+#     def display(self):
+#         print('car brand',self.brand)
+#         print('car model',self.model)
+# car1=car('tayota','fortuner')
+# car1.display()
+
+
+# class Book:
+#     def __init__(self,title,author):
+#         self.title=title
+#         self.author=author
+#     def details(self):
+#          print('The Book', self.title, 'is written by',self.author)
+# b1=Book('The Alchemist','Paulo Coelho')
+# b1.details()
+
+
+
+
+# class Employee:
+#     def __init__(self ,name,salary):
+#         self.name=name
+#         self.salary=salary
+#     def display(self):
+#         print('Employee Name',self.name)
+#         print('salary',self.salary)
+# e1=Employee('Malleswari',200000)
+# e1.display()
+# e1=Employee('Nagalakshmi',100000)
+# e1.display()
+
+
+# class Rectangle:
+#     def __init__(self ,length,breadth):
+#         self.Length=length
+#         self.breadth=breadth
+
+#     def area(self):
+#         print('length is',self.length)
+#         print('breadth is',self.breadth)
+# r1=Rectangle(3,7)
+# r1.area()
+
+        
+# class Laptop:
+#     def __init__(self,brand,price):
+#          self.brand=brand
+#          self.price=price
+#     def details(self):
+#          print('The Brand',self.brand, ' Price is',self.price)
+        
+# l1=Laptop('Dell',60000)
+# l1.details()
+
+
+# class BankAccount:
+#     def __init__(self,holder_Name,balence):
+#         self.holder_Name=holder_Name
+#         self.balence=balence
+
+
+#     def display(self):
+#        print('The Account holder Name is',self.holder_Name,'balence is',self.balence)
+
+# b1=BankAccount('Malleswari:', 200000)
+# b1.display()
+        
+        
+# class Mobile:
+#     def __init__(self,brand,model,price):
+#         self.brand=brand
+#         self.model=model
+#         self.price=price
+#     def display(self):
+#         print('Mobile brand:',self.brand)
+#         print('Mobile model:',self.model)
+#         print('Mobile price:',self.price)
+    
+# m1=Mobile('Apple','iphone 15pro',70000)
+# m1.display()
+
+
+# class MovieTicket:
+#     def __init__(self,movie_name,seat_num):
+#         self.movie_name=movie_name
+#         self.seat_num=seat_num
+    
+#     def ticket_details(self):
+#         print('your seat number  for the movie ', self.movie_name,'is',self.seat_num)
+
+# m1=MovieTicket('OG','A12')
+# m1.ticket_details()
+
+
+# class Dog:
+#     def __init__(self ,name,breed):
+#         self.name=name
+#         self.breed=breed
+
+#     def describing(self):
+#         print(f"{self.name} is a {self.breed} breed dog.")
+
+# d1=Dog('chinni','haski')
+# d1.describing()
+        
+
+
+
+        
+        
